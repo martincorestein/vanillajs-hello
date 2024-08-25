1db79d65ac74e0544b30b5a00ef259cc1c05fa39
martincorestein <103467287+martincorestein@users.noreply.github.com>
2024-08-25 06:30:51 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".excusa").innerHTML - excusas();
+  document.querySelector(".boton").addEventListener("click", () => {
+    document.querySelector(".excusa").innerHTML = excusas();
+  });
 };
+
+function excusas() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let quien = Math.floor(Math.random() * who.length);
+  let accion = Math.floor(Math.random() * action.length);
+  let que = Math.floor(Math.random() * what.length);
+  let donde = Math.floor(Math.random() * when.length);
+
+  return (
+    who[quien] + " " + action[accion] + " " + what[que] + " " + when[donde]
+  );
+}
